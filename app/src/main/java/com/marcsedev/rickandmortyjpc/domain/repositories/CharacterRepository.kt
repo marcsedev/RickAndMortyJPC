dff0adc9348cf4e4955d31ac99e71ba4a25ef377
MarcSogas <marc.sogas@gmail.com>
2023-04-14 15:50:38 +0200

    Add interface Character Repository

--- app/src/main/java/com/marcsedev/rickandmortyjpc/domain/repositories/CharacterRepository.kt
+++ app/src/main/java/com/marcsedev/rickandmortyjpc/domain/repositories/CharacterRepository.kt
@@ -1,4 +1,13 @@
 package com.marcsedev.rickandmortyjpc.domain.repositories
 
+import kotlinx.coroutines.flow.Flow
+import com.marcsedev.rickandmortyjpc.data.Result
+import com.marcsedev.rickandmortyjpc.domain.model.Characters
+import com.marcsedev.rickandmortyjpc.domain.model.Character
+
 interface CharacterRepository {
+
+    fun getAllCharacters(page: Int): Flow<Result<List<Characters>>>
+
+    suspend fun getCharacter(id: Int): Result<Character>
 }
